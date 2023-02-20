dea84ad96cbd7bbcba4b336519ec91079716c0bb
정선주 <tnver@naver.com>
2023-02-21 00:16:59 +0900

    [update] gradle dependencies 내에 tomcat 내용 추가돼있는 것 삭제

--- src/main/java/com/project/finalprj/AwesomeApplication.java
+++ src/main/java/com/project/finalprj/AwesomeApplication.java
@@ -2,7 +2,6 @@ package com.project.finalprj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication  //사용할 DataBase가 설정이 완료 되면 해당 주석 해제 후 위 어노테이션 삭제
 public class AwesomeApplication {
