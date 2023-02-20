8ccdf7408515766d76444e11f421afa6d65b677d
정선주 <tnver@naver.com>
2023-02-20 16:49:47 +0900

    [update] spring 초기 설정 내용 수정

--- src/main/java/com/project/finalprj/AwesomeApplication.java
+++ src/main/java/com/project/finalprj/AwesomeApplication.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
-//@SpringBootApplication  //사용할 DataBase가 설정이 완료 되면 해당 주석 해제 후 위 어노테이션 삭제
+@SpringBootApplication  //사용할 DataBase가 설정이 완료 되면 해당 주석 해제 후 위 어노테이션 삭제
 public class AwesomeApplication {
 
 	public static void main(String[] args) {
