c497a509be62e5e478bda3d569fb67b4693874cb
최병현 <>
2023-02-22 13:11:47 +0900

    [update] HeartController CartController url 연결 및 이름 변경

--- src/main/java/com/prj/web/awesome/user/controller/CartController.java
+++ src/main/java/com/prj/web/awesome/user/controller/CartController.java
@@ -1,4 +1,15 @@
 package com.prj.web.awesome.user.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping(value = "/user")
+@Controller
 public class CartController {
+
+    @GetMapping("/cart")
+    public String cart(){
+        return "html/user/userCart";
+    }
 }
--- src/main/java/com/prj/web/awesome/user/controller/HeartController.java
+++ src/main/java/com/prj/web/awesome/user/controller/HeartController.java
@@ -1,4 +1,15 @@
 package com.prj.web.awesome.user.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping(value = "/user")
+@Controller
 public class HeartController {
+
+    @GetMapping("/heart")
+    public String heart(){
+        return "html/user/userHeart";
+    }
 }
