044f0ebc552537b30e614f5c24d2d53afa2ec641
최병현 <>
2023-02-22 15:12:24 +0900

    [create] Community service 파일 추가

---
+++ src/main/java/com/prj/web/awesome/community/service/AttachmentService.java
@@ -0,0 +1,17 @@
+package com.prj.web.awesome.community.service;
+
+import com.prj.web.awesome.community.vo.AttachmentVO;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AttachmentService {
+
+//    List<AttachmentVO> selectList(AttachmentVO vo);
+//
+//    List<AttachmentVO> selectOne(AttachmentVO vo);
+//
+
+}
+
---
+++ src/main/java/com/prj/web/awesome/community/service/ReviewService.java
@@ -0,0 +1,7 @@
+package com.prj.web.awesome.community.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewService {
+}
