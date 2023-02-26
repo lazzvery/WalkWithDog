40efdca6d1bcb2a696069c33a18887067edcafc9
최병현 <>
2023-02-26 12:06:28 +0900

    [create, update] community notice db연동 및 페이지 th구성, ㅜㅐnotice.xml 파일 추가

--- src/main/java/com/prj/web/awesome/community/controller/NoticeController.java
+++ src/main/java/com/prj/web/awesome/community/controller/NoticeController.java
@@ -1,11 +1,14 @@
 package com.prj.web.awesome.community.controller;
 
+import com.prj.web.awesome.community.dto.NoticeDTO;
 import com.prj.web.awesome.community.service.NoticeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @RequestMapping(value = "/community")
 @Controller
@@ -16,6 +19,11 @@ public class NoticeController {
 
     @GetMapping("/notice")
     public String notice(Model model){
+
+        List<NoticeDTO> noticeList = noticeService.noticeList();
+
+        model.addAttribute("noticeList", noticeList);
+
         return "html/community/notice/communityNotice";
     }
 }
