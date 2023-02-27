696fa674b323557ef3b00808f560e1923a601430
mihyeon <xxlazvert@gmail.com>
2023-02-27 18:09:31 +0900

    [update] common.css에 magin 추가, order/item html, css 수정

--- src/main/java/com/prj/web/awesome/order/controller/OrderDetailController.java
+++ src/main/java/com/prj/web/awesome/order/controller/OrderDetailController.java
@@ -1,4 +1,16 @@
 package com.prj.web.awesome.order.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping(value = "/order")
+@Controller
 public class OrderDetailController {
+
+    @GetMapping("/orderDetail")
+    public String orderDetail(){
+        return "html/order/orderDetail";
+    }
+
 }
