f231a0b30f5e4f2f3b047b81c80c20d37ca6a26c
김민성 <>
2023-03-14 13:56:16 +0900

    [update] 관리자페이지 커뮤니티 매핑

--- out/production/resources/static/js/user/userFindId.js
+++ out/production/resources/static/js/user/userFindId.js
@@ -14,3 +14,30 @@ checkBox.addEventListener("click", (e) => {
         findIdContainer.children[2].style.display = "block"
     }
 });
+
+function idButton(event) {
+    let html = '';
+
+
+    let user_name = $('#user_name').val(),
+        user_email = $('#user_email').val(),
+        user_phone = $('#user_phone').val();
+
+
+
+    let sendData = "userEmail="+user_email+"userPhone="+user_phone+"userName"+user_name;
+
+    $.ajax({
+        type: "POST",
+        url: '/user/userFindId',
+        datatype: "text",
+        data: sendData,
+        success: function (text) {
+            html += <div>성공</div>;
+        },
+        error: function (xhr) {
+            alert('저장에 실패하였습니다. 다시 시도해주세요.');
+        }
+    });
+
+}
