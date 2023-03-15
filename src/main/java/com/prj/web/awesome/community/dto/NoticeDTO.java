8fd8b9a6bc829b4ecde79511aa876d9b4ab1453f
최병현 <>
2023-03-15 23:57:35 +0900

    [update, create] review 수정 및 사진추가

--- src/main/java/com/prj/web/awesome/community/dto/NoticeDTO.java
+++ src/main/java/com/prj/web/awesome/community/dto/NoticeDTO.java
@@ -7,7 +7,6 @@ import java.time.LocalDate;
 @Data
 public class NoticeDTO {
     private int notice_seq;
-    private int attachment_file_seq;
     private String notice_title;
     private String notice_content;
     private LocalDate notice_reg_date;
